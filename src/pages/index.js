d64cfd88c1f4b18615ac01ef08aaeba108d39924
Stéphane Roche <rochestephane@yahoo.fr>
2021-09-22 20:47:01 +0100

    Landing page (#30)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,82 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import Translate from '@docusaurus/Translate';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.scss';
-import HomepageFeatures from '../components/HomepageFeatures';
-import TdexLogo3d from '../../static/img/tdex_3d_logo.svg';
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import Translate from '@docusaurus/Translate'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import styles from './index.module.scss'
+import HomepageFeatures from '../components/HomepageFeatures'
+import Tdex3d from '../../static/img/tdex_3d.png'
+import TdexLogo from '../../static/img/tdex-logo.png'
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container text--center">
-        <TdexLogo3d className={styles.tdexLogo} />
-        <div className={styles.titles}>
-          <h1 className="hero__title">{siteConfig.title.toUpperCase()}</h1>
-          <h2 className="hero__subtitle">{siteConfig.tagline}</h2>
+    <header className={clsx('text--center', styles.heroBanner)}>
+      <div className="row row--no-gutters">
+        <div className="col col--4 col--offset-4">
+          <img src={Tdex3d} alt="Tdex3d"/>
+          <div className={styles.titles}>
+            <img src={TdexLogo} alt="TdexLogo"/>
+            <h2><span>TDEX</span> DEV PORTAL</h2>
+            <div className={styles.heroBtnContainer}>
+              <Link className="button button--outline" to="/docs">
+                <Translate>DOCS</Translate>
+              </Link>
+              <Link
+                className="button button--outline"
+                to="/blog">
+                <Translate>BLOG</Translate>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </header>
-  );
+  )
 }
-
 
 function HomepageCallToAction() {
   return (
     <section className={clsx(styles.call2action)}>
-      <div className="container text--center">
-        <h2 className="hero__title">How it works</h2>
-        <p>
-          Anyone can become a <b>market maker</b> in the TDEX network, putting reserves in various asset pairs forming a <b>Market</b> and gaining swap fees. It exposes a public reachable endpoint for traders to get the current <b>market price</b> and to accept atomic swap requests.
-        </p>
-        <h3 className="hero__title">Get started</h3>
-        <div className={`row ${styles.btnContainer}`}>
-          <Link className="button" to="/docs/trader/intro">
-            <Translate>For Traders</Translate>
-          </Link>
-          <Link
-            className="button"
-            to="/docs/provider/intro">
-            <Translate>For Liquidity Providers</Translate>
-          </Link>
+      <div className="container">
+        <div className="row">
+          <div className="col col--8 padding-horiz--lg">
+            <h3>HOW IT WORKS</h3>
+            <p>
+              Anyone can become a <b>market maker</b> in the TDEX network, putting reserves in various asset pairs forming
+              a <b>Market</b> and gaining swap fees. It exposes a public reachable endpoint for traders to get the current <b>market
+              price</b> and to accept atomic swap requests.
+            </p>
+          </div>
+          <div className="col col--4">
+            <div className={`row ${styles.btnContainer}`}>
+              <Link className="button button--outline" to="/docs/provider/intro">
+                <Translate>DOCS FOR LIQUIDITY PROVIDERS</Translate>
+              </Link>
+              <Link
+                className="button button--outline"
+                to="/docs/trader/intro">
+                <Translate>DOCS FOR TRADERS</Translate>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext()
   return (
     <Layout
       title={`${siteConfig.title} - Developer Portal`}
       description="Global P2P network for Liquid market makers and traders">
-      <HomepageHeader />
+      <HomepageHeader/>
       <main>
-        <HomepageFeatures />
-        <HomepageCallToAction />
+        <HomepageFeatures/>
+        <HomepageCallToAction/>
       </main>
     </Layout>
-  );
+  )
 }
